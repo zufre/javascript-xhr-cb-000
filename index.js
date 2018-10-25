c840268d82aa3d29bd6e74bb2722b582b865da0f
Cedric David <cedric.david@btopenworld.com>
2018-10-25 03:02:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,44 @@
+
+
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  req.addEventListener("load", showRepositories);
+  req.open("GET", "https://api.github.com/users/zufre/repos");
+  req.send();
+}
+function showRepositories(event, data) {
+  var repos = JSON.parse(this.responseText);
+  console.log(repos);
+  const repoList = `<ul>${repos
+    .map(
+      r =>
+        "<li>" +
+        r.name +
+        ' - <a href="#" data-repo="' +
+        r.name +
+        '" onclick="getCommits(this)">Get Commits</a></li>'
+    )
+    .join("")}</ul>`;
+  document.getElementById("repositories").innerHTML = repoList;
+}
+function getCommits(el) {
+  const name = el.dataset.repo;
+  const req = new XMLHttpRequest();
+  req.addEventListener("load", showCommits);
+  req.open("GET", "https://api.github.com/repos/zufre/" + name + "/commits");
+  req.send();
+}
+function showCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+        "<li><strong>" +
+        commit.commit.author.name +
+        "</strong> - " +
+        commit.commit.message +
+        "</li>"
+    )
+    .join("")}</ul>`;
+  document.getElementById("commits").innerHTML = commitsList;
+}
