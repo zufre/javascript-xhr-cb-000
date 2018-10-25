836dc234be20fdeb25d90d7eab556a4ec4b086d4
Cedric David <cedric.david@btopenworld.com>
2018-10-25 02:58:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,3 +21,10 @@ function showRepositories(event, data) {
     .join("")}</ul>`;
   document.getElementById("repositories").innerHTML = repoList;
 }
+function getCommits(el) {
+  const name = el.dataset.repo;
+  const req = new XMLHttpRequest();
+  req.addEventListener("load", showCommits);
+  req.open("GET", "https://api.github.com/repos/octocat/" + name + "/commits");
+  req.send();
+}
