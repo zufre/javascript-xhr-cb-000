0155b4b8689f12c90e7fddb9aafbaa3174adaf75
Cedric David <cedric.david@btopenworld.com>
2018-10-25 02:58:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ function getCommits(el) {
   const name = el.dataset.repo;
   const req = new XMLHttpRequest();
   req.addEventListener("load", showCommits);
-  req.open("GET", "https://api.github.com/repos/octocat/" + name + "/commits");
+  req.open("GET", "https://api.github.com/repos/zufre/" + name + "/commits");
   req.send();
 }
