cb2f82a5bc4122ec67689ac7106911a44a8728a8
Cedric David <cedric.david@btopenworld.com>
2018-10-25 02:36:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function getRepositories() {
   const req = new XMLHttpRequest();
-  req.open("GET", "https://api.github.com/users/octocat/repos");
+  req.open("GET", "https://api.github.com/users/zufre/repos");
   req.send();
 }
