6c699e65abb3275f7927dec0f72b4d3d1d0dafb8
Cedric David <cedric.david@btopenworld.com>
2018-10-25 02:54:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,9 +9,15 @@ function getRepositories() {
 function showRepositories(event, data) {
   var repos = JSON.parse(this.responseText);
   console.log(repos);
-  let repoList = `<ul>${repos
-    .map(r => "<li>" + r.name + "</li>")
+  const repoList = `<ul>${repos
+    .map(
+      r =>
+        "<li>" +
+        r.name +
+        ' - <a href="#" data-repo="' +
+        r.name +
+        '" onclick="getCommits(this)">Get Commits</a></li>'
+    )
     .join("")}</ul>`;
-
   document.getElementById("repositories").innerHTML = repoList;
 }
