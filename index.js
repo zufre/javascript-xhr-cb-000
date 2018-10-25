7a8c8a5177f9454e6bb00b1e8630e988096caa6e
Cedric David <cedric.david@btopenworld.com>
2018-10-25 02:43:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-function showRepositories(event, data) {
-  //this is set to the XMLHttpRequest object that fired the event
-  console.log(this.responseText);
-}
+
 
 function getRepositories() {
   const req = new XMLHttpRequest();
   req.addEventListener("load", showRepositories);
   req.open("GET", "https://api.github.com/users/octocat/repos");
   req.send();
+}
+function showRepositories(event, data) {
+  console.log(this.responseText);
+  let repoList = "<ul>";
+  for (var i = 0; i < this.responseText.length; i++) {
+    repoList += "<li>" + this.responseText[i]["name"] + "</li>";
+  }
+  repoList += "</ul>";
+  document.getElementById("repositories").innerHTML = repoList;
 }
